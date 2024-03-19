d1dbe6772a8874ca072bc379488c932fc3292782
Elkin Mejía <111328546+elkime@users.noreply.github.com>
2024-03-19 13:43:50 -0500

    ok working

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const morgan = require("morgan");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-//require("dotenv").config(); 
-
 const port = process.env.PORT || 3000;
 
 const router = require("./routes");
@@ -42,6 +40,3 @@ app.listen(port, () => {
 
 
 
-//    mongodb+srv://giovazzaro:kPQJUx9KRTf1pTs9@cluster0.takrkmp.mongodb.net/
-
-//   mongodb+srv://giovazzaro:kPQJUx9KRTf1pTs9@cluster0.takrkmp.mongodb.net/db_contacts?retryWrites=true&w=majority&appName=Cluster0
